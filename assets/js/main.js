0733f49b9b8dda1d5e24c9c92553013d7d4ef7d8
Ankit Jangir <ankit2000jangir@gmail.com>
2021-05-13 17:48:43 +0530

    update and remove certi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: iPortfolio - v1.2.0
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
@@ -101,94 +96,7 @@
     return false;
   });
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  // Skills section
-  $('.skills-content').waypoint(function() {
-    $('.progress .progress-bar').each(function() {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-  }, {
-    offset: '80%'
-  });
-
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
-  // Initi AOS
-  AOS.init({
-    duration: 1000,
-    easing: "ease-in-out-back"
-  });
+  
 
 })(jQuery);
 
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-
-   const mvr = document.getElementById('mover');
-  const btn = document.getElementById('move');
-
-  if(btn.className == "move day"){
-    mvr.style.transform = "translateY(-24px)";
-    btn.className = "move night";
-    btn.style.boxShadow = "0px 0px 16px rgba(255, 255, 255, 0.25)";
-    body.style.backgroundColor = "#0E0E0E";
-    body.style.Color = "#FFFFFF";
-  }else{
-    mvr.style.transform = "translateY(0)";
-    btn.className = "move day";
-    btn.style.boxShadow = "0px 0px 16px rgba(0, 0, 0, 0.25)";
-    body.style.backgroundColor = "white";
-  }
-}
